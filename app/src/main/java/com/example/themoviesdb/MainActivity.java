f2fefc829ead6a457181ee787e903109d85d24f7
barcenas.proteco@gmail.com <Edgar1515>
2021-01-22 06:55:22 -0600

    Navigation Component

--- app/src/main/java/com/example/themoviesdb/MainActivity.java
+++ app/src/main/java/com/example/themoviesdb/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.themoviesdb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        if (navHostFragment != null) {
+            NavigationUI.setupWithNavController(navView, navHostFragment.getNavController());
+        }
+
+
     }
+
 }
